d997b979a5d37a2736a8a18d9be8ec1857601f17
Kaik0405 <159601515+Kaik0405@users.noreply.github.com>
2024-02-22 14:47:31 -0500

    h2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Text;
@@ -12,6 +13,7 @@ namespace LibreriaUnity
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world1");
         }
     }
 }
